ee98384f55206c790736d0200c218936f1766de7
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-06-23 14:12:39 +0530

    Search route - updated

--- app/api/users/search/route.js
+++ app/api/users/search/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
-import User from "@/models/User";
+import User from "@/lib/models/Users";
 import jwt from "jsonwebtoken";
 
 export async function GET(request) {
